5d447fb7c7d20a5449292d11744a65b849f68adc
David Robertson <david.m.robertson1@gmail.com>
2014-09-23 17:29:30 +0100

    Making the TreePair test script more user-friendly

--- scripts/test_pair.py
+++ scripts/test_pair.py
@@ -1,7 +1,13 @@
-import sys, os
-sys.path.insert(0, os.path.abspath('.'))
+#Naughty hack to make the thompson package importable. Adds the root directory to sys.path
+import sys
+from pathlib import Path
+
+root = Path(__file__).parents[1]
+sys.path.insert(0, str(root))
 
 from thompson.tree_pair import TreePair
+
+id = TreePair("0", "0")
 
 a = TreePair("11010100100", "11010100100", "1 6 5 2 3 4")
 x = a
@@ -9,7 +15,9 @@ x.render(filename='a_power_1.svg')
 for i in range(5):
 	x = x * a
 	x.render(filename='a_power_{}.svg'.format(i+2))
+	print('a to the power {} is the identity? {}'.format(i+2, x == id))
 
 #TODO: __pow__: method of squares?
+#Does one big chain of multiplications work?
 y = a * a * a * a * a * a
-print('a**6 == 1?', x == TreePair("0", "0"), y == TreePair("0", "0"))
+print('a**6 == 1?', y == id)
