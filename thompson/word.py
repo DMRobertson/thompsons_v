96ad971a2840614c66101d1c20b41eea5ba591b7
David Robertson <david.m.robertson1@gmail.com>
2014-09-26 08:08:43 +0100

    Beginning work on a word class

---
+++ thompson/word.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+class Word(list):
+	r"""Represents an element of :math:`V_{n, r}`. These are words written in the alphabet \{x_1,\dotsc, x_r, \alpha_1, \dotsc, \alpha_n, \lambda\} (Section 2.3).
+	"""
+	def __init__(self, arity, alphabet_size):
+		r"""When creating a word, you must specify the arity *n* and the forest size *r*."""
+		super().__init__(self)
+		
+		self.arity = arity
+		self.alphabet_size = alphabet_size
+	
+	#Internal scheme: positive integers  i correspond to x_i
+	#                 negative integers -i correspond to alpha_i
+	#                 zero corresponds to lambda
+	
+	def letter(self, index=1):
+		r"""Writes the letter :math:`x_\text{index}` at the end of this word.
+		
+		:raises ValueError: if index is not in the range 1, ..., *alphabet_size*."""
+		if not 1 <= index <= self.alphabet_size:
+			raise ValueError("Index {} is not in the range 1 to alphabet size {}".format(index, self.alphabet_size))
+		self.append(index)
+		return self
+	
+	x = letter
+	
+	def descend(self, index, power=1):
+		r"""Writes the descend operator :math:`\alpha_\text{index}` at the end of this word *power* times.
+		
+		:raises ValueError: if index is not in the range 1, ..., *arity*."""
+		if not 1 <= index <= self.arity:
+			raise ValueError("Index {} is in the range 1 to arity {}".format(index, self.arity))
+		for _ in range(power):
+			self.append(-index)
+		return self
+	
+	alpha = descend
+	
+	def contract(self):
+		r"""Writes the contraction operator :math:`\lambda` at the end of this word."""
+		self.append(0)
+		return self
+	
+	lambda_ = contract
+	
+	@classmethod
+	def from_string(cls, str, arity = 2, alphabet_size = 1):
+		"""Creates a word object from a space separated string of letters. The *arity* and *alphabet_size* are determined automatically, but they can be overridden if necessary. No validation is performed on the new word object.
+		
+			>>> w = Word.from_string("x a1 a1 a1 x a1 a1 a2 L a2")
+			>>> print(w)
+			x a1 a1 a1 x a1 a1 a2 L a2
+		
+		"""
+		output = str.lower().split()
+		for i, string in enumerate(output):
+			char = string[0]
+			if char in 'x':
+				value = 1 if len(string) == 1 else int(string[1:])
+				alphabet_size = max(alphabet_size, value)
+				output[i] = value
+			elif char in 'a':
+				value = int(string[1:])
+				arity = max(arity, value)
+				output[i] = -value
+			elif char in 'l': #ell for lambda
+				output[i] = 0
+		result = cls(arity, alphabet_size)
+		result.extend(output)
+		return result
+	
+	def __str__(self):
+		return " ".join(self._char(i) for i in self)
+	
+	def _char(self, symbol):
+		if symbol > 0:
+			if self.alphabet_size == 1:
+				return 'x'
+			return 'x' + str(symbol)
+		elif symbol < 0:
+			return 'a' + str(-symbol)
+		elif symbol == 0:
+			return 'L'
+	
+	def validate(self):
+		"""Raises a ValueError if this word is invalid. (Prop 2.12)
+		
+			>>> w = Word.from_string("x a1 a2 L")
+			>>> w.validate()
+			Traceback (most recent call last):
+				...
+			ValueError: Word is invalid at symbol #4 (L).
+			>>> w = Word.from_string("x a1 a2 x a2")
+			>>> w.validate()
+			Traceback (most recent call last):
+				...
+			ValueError: Word is invalid: valency is 2 (should be 1).
+		"""
+		valency = 0
+		for i, symbol in enumerate(self):
+			valency += self._valency_of(symbol)
+			if valency <= 0:
+				raise ValueError("Word is invalid at symbol #{} ({}).".format(i+1, self._char(symbol)))
+		if valency != 1:
+			raise ValueError("Word is invalid: valency is {} (should be 1).".format(valency))
+	
+	def _valency_of(self, symbol):
+		"""Returns the valency of a symbol as per Def 2.11."""
+		if symbol > 0: #letter x_i
+			return 1
+		if symbol < 0: #descenders alpha
+			return 0
+		if symbol == 0:#contractor lambda
+			return 1 - self.arity
+	
+	def standardise(self):
+		"""Reduces this word to its standard form as defined in Rem 3.3.
+		:raises IndexError: if the first character is not a letter x_i.
+		:raises ValueError: if a lambda is found with insufficient arguments to its left.
+		
+			>>> w = Word.from_string("x a1 a2 a1 x a1 a2 a2 L a2 a1")
+			>>> w.standardise()
+			>>> print(w)
+			x a1 a2 a2 a1
+		"""
+		subwords = []
+		i = 0
+		while i < len(self):
+			symbol = self[i]
+			if symbol > 0: #Letter
+				subwords.append([symbol])
+			
+			elif symbol == 0: #Lambda
+				if len(subwords) < self.arity:
+					raise ValueError("Lambda contraction expected {} args but received only {}.".format(
+						self.arity, len(subwords)
+					))
+				width = self._contract(subwords, i)
+				if width is not None:
+					del self[i - width + 1: i + 1]
+					i -= width + 1 #subtract 1: loop will add one on at the end
+			
+			else: #Alpha
+				subwords[-1].append(symbol)
+				
+			i += 1
+	
+	def _contract(self, subwords, lambda_position):
+		"""Attempts to contract the last *n* words in *subwords*, where *n* is the current word's arity.
+		
+		Returns the difference between the word's length before and after contraction.
+		"""
+		start = subwords[-self.arity][:-1]
+		assert start[0] > 0 #is a letter
+		len_start = len(start)
+		invalid = False
+		
+		for j, word in enumerate(subwords[-self.arity:]):
+			if not (len(word) == len_start + 1
+			  and word[:len_start] == start
+			  and word[-1] == -(j + 1)): #alpha_{j+1}
+				invalid = True
+				break
+		
+		if invalid:
+			subwords[-self.arity] = sum(subwords[-self.arity:]) 
+			del subwords[-self.arity + 1:]
+			return None
+		else:
+			subwords[-self.arity] = start
+			del subwords[-self.arity + 1:]
+			width = len(start) * (self.arity - 1) + self.arity + 1
+			return width
