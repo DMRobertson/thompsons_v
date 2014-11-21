68e878595c410c1387968d99162a640dc6c8f023
David Robertson <david.m.robertson1@gmail.com>
2014-11-21 16:45:18 +0000

    More details of ponds worked out

--- scripts/debug.py
+++ scripts/debug.py
@@ -18,14 +18,19 @@ print(phi_i)
 print(phi_i.quasinormal_basis())
 
 basis = phi_i.quasinormal_basis()
-for gen in basis:
-	print(gen, phi_i.orbit_type(gen, basis)[0])
+for gen in phi_i.semi_infinite_end_points():
+	type = phi_i.orbit_type(gen, basis)[0]
+	if not type.is_type_C():
+		continue
+	print('\n\n', gen, type)
+	for i in range(-10, 11):
+		print('\tpower', i, '=', phi_i.repeated_image(gen, i))
 
 # rho_i = psi_i.test_conjugate_to(phi_i)
 
-u = Word('x a1 a1 a1 a1 a1 a1 a1 a2', (2, 1))
-print(phi_i.repeated_image(u, 4))
-v = Word('x a2 a2 a1 a1 a2', (2, 1))
-solns = phi_i.share_orbit(u, v)
+# u = Word('x a1 a1 a1 a1 a1 a1 a1 a2', (2, 1))
+# print(phi_i.repeated_image(u, 4))
+# v = Word('x a2 a2 a1 a1 a2', (2, 1))
+# solns = phi_i.share_orbit(u, v)
 
-print(solns)
+# print(solns)
