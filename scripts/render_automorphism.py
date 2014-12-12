065c75f365d4e8a0e54e1bab703cb142f1dc3737
David Robertson <david.m.robertson1@gmail.com>
2014-12-12 15:26:50 +0000

    Render a periodic QNB with graphviz

--- scripts/render_automorphism.py
+++ scripts/render_automorphism.py
@@ -1,54 +1,42 @@
 from test import setup_script
 setup_script(__file__)
 
+import os
+
 from thompson import *
 from thompson.examples import *
-from thompson.word import format
+from thompson.word import format as fmt
 
-"""A hack which turns a basis into a string of 1s and 0s for use with older code."""
+def format(word):
+	return fmt(word).replace(' ', '_')
 
-def split_basis(basis):
-	assert basis.is_basis()
-	strings = []
-	for i in range(1, basis.signature.alphabet_size + 1):
-		part = Generators(basis.signature, (w for w in basis if w[0] == i))
-		print(part)
-		strings.append(to_binary_string(part, basis.signature.arity, i))
-	return strings
-
-def to_binary_string(basis, arity, root_number):
-	if len(basis) == 0:
-		return '0'
-	
-	string = []
-	path = (root_number,)
-	
-	print(format(path))
-	for word in basis:
-		print('looking for', word)
-		# 1. Descend to word
-		while len(word) > len(path):
-			path = path + (-1,)
-			string.append('1')
-			print(format(path))
-		# 2. Are we at the word we want to be at?
-		while word != path:
-			path = next(path, arity)
-		print(format(path))
-		string.append('0')
-		path = next(path, arity)
-		print(format(path))
+def render_basis(aut, name):
+	'''only works for quasinormal'''
+	basis = aut.quasinormal_basis()
+	with open(name + '.dot', 'wt') as f:
+		f.write('strict digraph G {\n')
 		
-	return ''.join(string)
-
-def next(path, arity):
-	while -path[-1] == arity:
-		path = path[:-1]
-	return path[:-1] + (path[-1]-1,)
+		for i in range(1, basis.signature.alphabet_size+1):
+			f.write('\tsubgraph cluster{} {{\n'.format(i))
+			# f.write('\t\tstyle=filled;\n'.format(i))
+			# f.write('\t\tcolor=lightgrey;\n'.format(i))
+			f.write('\t\tx{};\n'.format(i))
+			
+			for j, word in enumerate(basis):
+				if word[0] == i:
+					f.write('\t\t')
+					for k in range(1, len(word)):
+						head, _ = word.split(k)
+						f.write(format(head) + ' -> ')
+					f.write(format(word) + ';\n')
+					j2 = basis.index(aut.image(word, inverse=True))
+					f.write('\t\t{}[label = "{};{}"];\n'.format(format(word), j+1, j2+1))
+			f.write('\t}\n')
+		f.write('}\n')
+	
+	os.system("dot -T png -o {}.png {}.dot".format(name, name))
 
 if __name__ == '__main__':
-	aut = cyclic_order_six
-	print(aut)
-	print(split_basis(aut.domain))
-	aut = Automorphism.from_file('QNB_size_206.aut')
-	print(split_basis(aut.quasinormal_basis()))
+	render_basis(Automorphism.from_file('QNB_size_206.aut'), 'QNB_size_206')
+	render_basis(Automorphism.from_file('QNB_size_344.aut'), 'QNB_size_344')
+	render_basis(cyclic_order_six, 'cyclic_order_six')
