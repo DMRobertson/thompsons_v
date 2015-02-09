9bbf14c9c5b24b2e51e9ae03cf8d0967925a0ffc
David Robertson <david.m.robertson1@gmail.com>
2015-02-09 14:13:30 +0000

    Brief intro to package

--- thompson/__init__.py
+++ thompson/__init__.py
@@ -3,6 +3,6 @@
 Importing ``*`` from this module will add the :class:`~thompson.word.Word`, :class:`~thompson.generators.Generators`, and :class:`~thompson.automorphism.Automorphism` classes to the current namespace. The :mod:`~thompson.examples` module is **not** imported.
 """
 
-from .word         import Word
+from .word         import Word, Signature
 from .generators   import Generators
 from .automorphism import Automorphism
