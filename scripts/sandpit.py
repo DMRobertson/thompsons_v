39c7ad1889ac0e7d360a7d52b5cb9635967234d5
David Robertson <david.m.robertson1@gmail.com>
2015-03-19 19:02:54 +0000

    WIP trying to highlight repellers and attractors

--- scripts/sandpit.py
+++ scripts/sandpit.py
@@ -3,38 +3,53 @@ setup_script(__file__)
 
 """A place to play around with the package."""
 
-from thompson import Automorphism, Word
+from thompson import *
 olga_f = Automorphism.from_file('../thompson/examples/olga_f.aut')
 
 head = r'''\begin{tikzpicture}[level distance=8mm,
 	level 1/.style={sibling distance=25mm},
-	level 2/.style={sibling distance=1cm},
-	level 3/.style={sibling distance=5mm}]
+	level 2/.style={sibling distance=15mm},
+	level 3/.style={sibling distance=10mm},
+	level 4/.style={sibling distance=5mm},
+	characteristic/.style={red}]
 \usetikzlibrary{positioning}
 '''
 
 tail = r'''\end{tikzpicture}'''
 
 def dump_tikz_code(aut, filename, domain=None, name=''):
+	"""Saves instructions in the given *filename* for TikZ to draw the automorphism *aut*. The leaves of the domain tree are the elements of *domain*."""
+	#1. Setup
 	if domain is None:
 		domain = aut.domain
 	range = aut.image_of_set(domain)
+	#Compute X = L(domain \intersect range)
+	X = domain.copy()
+	X += range
+	finished = False
+	while not finished:
+		i, j = X.test_free()
+		if i == j == -1:
+			finished = True
+		else:
+			del X[j]
+	X.sort()
+	
 	with open(filename, 'wt') as f:
 		f.write(head)
-		basis_to_tree(domain, range, f, name='domain')
+		basis_to_tree(aut, domain, range, X, f, name='domain')
 		
 		y = max(len(w) for w in domain) - 1
 		z = max(len(w) for w in domain) - 1
 		depth = -min(y, z)/2
 		
 		coordend = r'\textwidth, ' + str(depth*8) + 'mm)'
-		print(r'\draw[->, thick] (0.25' + coordend, '-- node[auto]{', name, r'} (0.35' + coordend, ';\n', file=f)
-		basis_to_tree(range, domain, f, name='range', extra=r'xshift=0.6\textwidth')
+		print(r'\draw[->, thick] (0.25' + coordend, '-- node[auto]{$', name, r'$} (0.35' + coordend, ';\n', file=f)
+		
+		basis_to_tree(aut, range, domain, X, f, name='range', extra=r'xshift=0.6\textwidth')
 		f.write(tail)
 
-
-
-def basis_to_tree(basis, other, f, name, extra=''):
+def basis_to_tree(aut, basis, other, intersection, f, name, extra=''):
 	f.write(r'\begin{scope}[' + extra + ']\n')
 	f.write(r'\coordinate (' + name + ') {} \n')
 	depth = 1
@@ -54,7 +69,12 @@ def basis_to_tree(basis, other, f, name, extra=''):
 		
 		f.write('\t' * (depth) + 'child {')
 		if leafnum is not None:
-			f.write('node{' + str(leafnum) + '}')
+			f.write('node ')
+			print(path, intersection)
+			print(aut.orbit_type(path, intersection))
+			if aut.orbit_type(path, intersection)[0].is_type_B():
+				f.write('[characteristic] ')
+			f.write('{' + str(leafnum) + '}')
 			if dashed.pop():
 				f.write(' edge from parent[dashed] ')
 			f.write('}')
@@ -70,4 +90,6 @@ def basis_to_tree(basis, other, f, name, extra=''):
 	print(';', file=f)
 	print(r'\end{scope}', file=f)
 
-dump_tikz_code(olga_f, 'test.tikz')
+domain = olga_f.domain.copy()
+domain.expand(6).expand(2)
+dump_tikz_code(olga_f, r'H:\robertson\Reading\Salazar-Díaz revealing pairs\test.tikz', domain, 'f')
