47fc1adc9f86359af1ef07b4f3284a3e949fec9a
David Robertson <david.m.robertson1@gmail.com>
2016-03-02 00:34:52 +0000

    Can draw (n,r) forests
    need to fix positioning though

--- thompson/drawing/treepair.py
+++ thompson/drawing/treepair.py
@@ -1,22 +1,23 @@
+from thompson import Generators
 from thompson.examples import random_automorphism
 import pkg_resources
 
-def write_tikz_code(aut, dest='tikz_generated.tex'):
+def write_tikz_code(aut, dest='tikz_generated.tex', horiz=True):
 	header = pkg_resources.resource_filename('thompson.drawing', 'tikz_template_head.tex')
 	footer = pkg_resources.resource_filename('thompson.drawing', 'tikz_template_foot.tex')
 	with open(dest, 'wt') as f:
 		append_to(header, f)
+		f.write( "[component direction={}]\n".format( "right" if horiz else "down" ) )
+		f.write("\graph [nodes=caret] {\n")
 		
-		for i in range(0, aut.signature.alphabet_size):
-			f.write( "\tdx{} [root]\n".format( i+1 ) )
-			f.write( "\trx{} [root]\n".format( i+1 ) )
+		domain = [ (w, i + 1) for (i, w) in enumerate(aut.domain) ]
+		write_basis(f, domain, True)
+		f.write("\n")
 		
-		write_basis(f, aut.domain, True)
-		write_basis(f, aut.range, False)
+		range  = [ (w, i + 1) for (i, w) in enumerate(aut.range)  ]
+		range.sort()
+		write_basis(f, range, False)
 		
-		
-
-				
 		append_to(footer, f)
 
 def append_to(src, dest):
@@ -24,32 +25,69 @@ def append_to(src, dest):
 		for line in f:
 			dest.write(line)
 
-def name(word, domain):
+def name(word, domain, options):
 	prefix = 'd' if domain else 'r'
-	return prefix + str(word).replace(" ", "")
+	label = prefix + str(word).replace(" ", "")
+	pairs = []
+	for key, value in options.items():
+		if value is None:
+			pairs.append(key)
+		else:
+			pairs.append('{}={}'.format(key, value))
+	if pairs:
+		label += " [{}]".format( ', '.join(pairs) )
+	return label
+
+def pairs(iterator):
+	this = next(iterator)
+	while True:
+		try:
+			that = next(iterator)
+		except StopIteration:
+			yield (this, None)
+			break
+		else:
+			yield (this, that)
+			this = that
 
 def write_basis(f, basis, domain):
-	for i, word in enumerate(basis):
-		for subword in word.subwords():
-			node_name = name(subword, domain=True)
-			first = len(subword) == 1
-			last  = len(subword) == len(word)
+	subgraph_start = ( "{0} [name={0}]".format("domain" if domain else "range") )
+	subgraph_start += " // [tree layout, grow=down, component direction=right, component sep=2.5em] {\n"
+	f.write(subgraph_start)
+	
+	placed = set()
+	for i, (word, label) in enumerate(basis):
+		first = True
+		for parent, child in pairs(word.subwords()):
+			if child in placed:
+				continue
+			options = {}
+			
 			f.write("\t")
-			f.write(node_name)
-			if len(subword) > 1:
-				index = -subword[-1]
-				f.write( " [desired child index={}] ".format(index) )
-			if last:
-				f.write( " [leaf, as=${}$],\n".format(i+1) )
-
-# this almost works
-"""		
-		for i, word in enumerate(sorted(aut.range)):
-			names = [name(subword, domain=False) for subword in word.subwords()]
-			names[-1] += " [leaf, as=${}$],\n".format(i+1)
-			f.write("\t" + " -- ".join(names))
-"""
+			if not first:
+				f.write("\t-- ")
+			else:
+				first = False
+			
+			if len(parent) == 1:
+				options["root"] = None
+			if parent == word:
+				options["leaf"] = None
+				options["label"] = "below:" + str(label)
+			
+			f.write(name(parent, domain, options))
+			placed.add(parent)
+			
+			if parent == word:
+				if i == len(basis) - 1:
+					f.write(";")
+				else:
+					f.write(",")
+			f.write("\n")
+	
+	f.write("};\n")
 
 if __name__ == "__main__":
 	f = random_automorphism()
+	print(f)
 	write_tikz_code(f)
